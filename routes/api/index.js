a5d8558f5e78f338c6daecda9036351e245ffa30
rethissunstar <rethissunstar@gmail.com>
2023-07-28 20:30:36 -0400

    updated the controller to not create a reaction without a post

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const appRoutes = require('./appRoutes');
 const userRoutes = require('./userRoutes');
+const reactionRoutes = require('./reactionRoutes'); // Add this line
+const thoughtRoutes = require('./thoughtRoutes'); // Add this line
 
 router.use('/apps', appRoutes);
 router.use('/users', userRoutes);
+router.use('/reactions', reactionRoutes); // Add this line
+router.use('/thoughts', thoughtRoutes); // Add this line
 
 module.exports = router;
